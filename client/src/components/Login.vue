c7da24c1b705c77936192a1c7aa9c1fdf25440dc
Muhriddin <muhriddinmusoxonov@gmail.com>
2026-01-02 22:40:21 +0500

    register and login page

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -9,28 +9,26 @@
   -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-orange-600">Sign in to your account</h2>
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-orange-600">Login</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="#" method="POST">
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-orange-600">Email address</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Gmail Address..." />
           </div>
         </div>
 
         <div>
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-orange-600">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-orange-600 hover:text-orange-400">Forgot password?</a>
-            </div>
           </div>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Password..." />
           </div>
+          <div class="text-sm flex justify-end items-center mt-3">
+              <a href="#" class="font-semibold text-orange-600 hover:text-orange-400">Forgot password?</a>
+            </div>
         </div>
 
         <div>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,19 +1,4 @@
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-900">
-    <body class="h-full">
-    ```
-
-    Regstern update
-
-      fullname
-      email
-      password
-      phone
-  -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-orange-600">Register Form</h2>
@@ -23,22 +8,25 @@
       <form class="space-y-6" action="#" method="POST">
         <div>
           <!-- <label for="fullName" class="block text-sm/6 font-medium text-orange-600">Full Name</label> -->
-          <div class="mt-2">
-            <input type="text" name="fullName" id="fullName" autocomplete="fullName" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Full Name..." />
+          <div class="mt-2 flex justify-center items-center">
+            <i class="fa-solid fa-user inline text-orange-600 text-2xl mx-2"></i>
+            <input type="text" name="fullName" id="fullName" autocomplete="fullName" required class="w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Full Name..." />
           </div>
         </div>
 
         <div>
           <!-- <label for="email" class="block text-sm/6 font-medium text-orange-600">Email Address</label> -->
-          <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Email Addres..."/>
+          <div class="mt-2 flex justify-center items-center">
+            <i class="fa-solid fa-envelope inline text-orange-600 text-2xl mx-2"></i>
+            <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Gmail Address..."/>
           </div>
         </div>
 
         <div>
           <!-- <label for="phoneNumber" class="block text-sm/6 font-medium text-orange-600">Phone Number</label> -->
-          <div class="mt-2">
-            <input type="phoneNumber" name="phoneNumber" id="phoneNumber" autocomplete="phoneNumber" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Phone Number..."/>
+          <div class="mt-2 flex justify-center items-center">
+            <i class="fa-solid fa-phone inline text-orange-600 text-2xl mx-2"></i>
+            <input type="phoneNumber" name="phoneNumber" id="phoneNumber" autocomplete="phoneNumber" required class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Phone Number..."/>
           </div>
         </div>
 
@@ -46,17 +34,18 @@
           <div class="flex items-center justify-between">
             <!-- <label for="password" class="block text-sm/6 font-medium text-orange-600">Password</label> -->
           </div>
-          <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Password..." />
+          <div class="mt-2 flex justify-center items-center">
+            <i class="fa-solid fa-key inline text-orange-600 text-2xl mx-2"></i>
+            <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Password..." />
           </div>
         </div>
 
-        <div>
+        <div class="w-60 ml-22">
           <button type="submit" class="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-orange-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Sign in</button>
         </div>
       </form>
 
-      <p class="mt-10 text-center text-sm/6 text-gray-400">
+      <p class="mt-10 text-center text-sm/6 text-gray-400 ml-8">
         Not a member?
         {{ ' ' }}
         <a href="#" class="font-semibold text-orange-600 hover:text-indigo-300">Start a 14 day free trial</a>
@@ -64,26 +53,3 @@
     </div>
   </div>
 </template>
-
-
-
-
-
-
-
-
-
-
-<!-- <template>
-  <div>
-    <h1>Register Page</h1>
-  </div>
-</template>
-<script>
-export default {
-
-}
-</script>
-<style>
-
-</style> -->
