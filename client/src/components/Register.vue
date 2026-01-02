00cc8d6c06bc7e72d272ed889ee7bb7abede9c7f
Muhriddin <muhriddinmusoxonov@gmail.com>
2026-01-02 11:00:50 +0500

    register page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,30 +6,48 @@
     <html class="h-full bg-gray-900">
     <body class="h-full">
     ```
+
+    Regstern update
+
+      fullname
+      email
+      password
+      phone
   -->
   <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-orange-600">Sign in to your account</h2>
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-orange-600">Register Form</h2>
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form class="space-y-6" action="#" method="POST">
         <div>
-          <label for="email" class="block text-sm/6 font-medium text-orange-600">Email address</label>
+          <!-- <label for="fullName" class="block text-sm/6 font-medium text-orange-600">Full Name</label> -->
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input type="text" name="fullName" id="fullName" autocomplete="fullName" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Full Name..." />
+          </div>
+        </div>
+
+        <div>
+          <!-- <label for="email" class="block text-sm/6 font-medium text-orange-600">Email Address</label> -->
+          <div class="mt-2">
+            <input type="email" name="email" id="email" autocomplete="email" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Email Addres..."/>
+          </div>
+        </div>
+
+        <div>
+          <!-- <label for="phoneNumber" class="block text-sm/6 font-medium text-orange-600">Phone Number</label> -->
+          <div class="mt-2">
+            <input type="phoneNumber" name="phoneNumber" id="phoneNumber" autocomplete="phoneNumber" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Phone Number..."/>
           </div>
         </div>
 
         <div>
           <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-orange-600">Password</label>
-            <div class="text-sm">
-              <a href="#" class="font-semibold text-orange-600 hover:text-orange-400">Forgot password?</a>
-            </div>
+            <!-- <label for="password" class="block text-sm/6 font-medium text-orange-600">Password</label> -->
           </div>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-orange-600 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" placeholder="Password..." />
           </div>
         </div>
 
