72f36433eaa6fec224a06f541da8ecba2445bdee
Muhriddin <muhriddinmusoxonov@gmail.com>
2026-02-07 15:28:09 +0500

    add product listes

---
+++ client/src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white">
+        <div class="group relative">
+          <div class="">
+            <img :src="product.image_url[0]" class="aspect-square w-full object-cover group-hover:border border-orange-400 rounded-2xl p-4 lg:aspect-auto lg:h-80" />
+          </div>
+          <div class="mt-4 flex justify-between">
+            <div>
+              <h3 class="text-sm text-gray-700">
+                <a :href="product.href">
+                  <span aria-hidden="true" class="absolute inset-0"></span>
+                  {{ product.name }}
+                </a>
+              </h3>
+              <!-- <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p> -->
+            </div>
+            <p class="text-sm font-medium text-gray-900">{{ product.price }}</p>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object,
+        required: true
+      }
+    }
+  }
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
     <h1 class="text-red">Home</h1>
-    {{ data }}
+    <!-- {{ data }} -->
+      <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+
+        <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <ProductCard v-for="product in data" :product="product" :key="product.slug"/>
+      </div>
+      </div>
   </div>
 </template>
 <script>
+import ProductCard from '@/components/ProductCard.vue';
 import { mapState } from 'vuex';
 
 
 export default {
+
+  components: {
+    ProductCard
+  },
+
   computed: {
     ...mapState({
       data: state => state.products.data,
